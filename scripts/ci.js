3e7f2ec4599a452d99141c315be7e99b20df878f
Jess Harrell <jharrell@pillartechnology.com>
2018-06-11 15:53:16 -0500

    RED one test failing on purpose. hack in place to detect

---
+++ scripts/ci.js
@@ -0,0 +1,51 @@
+const kill = require('tree-kill');
+const spawn = require('child_process').spawn;
+// const await = require('await');
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+let testStatus = 0;
+function markTestFailed(){
+    testStatus = 1;
+}
+
+function startServer() {
+    const server = spawn('npm', ['start']);
+    return server;
+}
+
+function startTests() {
+    console.log("starting tests");
+    const tests = spawn('npm', ['run', 'test:ci']);
+    tests.stdout.on('data', function(data) {
+        console.log(data.toString());
+    });
+    tests.stderr.on('data', function(data) {
+        markTestFailed();
+        console.log(data.toString());
+    });
+    return tests;
+}
+
+function setServerToDieWhenTestsComplete(server, tests) {
+    const killServer = () => {
+        kill(server.pid);
+    };
+
+    tests.on('close', () => {
+        killServer();
+        console.log("tests ran now exit based on tests status");
+        process.exit(testStatus);
+    });
+    tests.on('error', () => {
+        killServer();
+        console.log("tests failed to run!!!");
+    });
+}
+
+const server = startServer();
+sleep(10000).then(() => {
+    const tests = startTests();
+    setServerToDieWhenTestsComplete(server, tests);
+})
